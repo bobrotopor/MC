42081408a25955af39fa46eed401f5778c3fbcf6
bobrotopor <kaster114@gmail.com>
2024-04-11 22:15:29 +0300

    промежуточный коммит x51

--- x51/lab2_2_x51.c
+++ x51/lab2_2_x51.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <intrins.h>
 #include <string.h>
+
 #include "KEYBOARD.H"
 #include "SPI.H"
 #include "LCD_HD44780.H"
@@ -13,157 +14,133 @@
 #include "I2C.H"
 #include "SHIFT.H"
 
-int x = 0;
-int z = 0; 
-int s = 0; 
-int i = 0;
-unsigned char y = 100;
-unsigned char a = 0;
-unsigned char key = 0;
+unsigned char task;
 
-#define CLOCK 22110000
-#define TIMER_FREQ_HZ 1000
-#define RELOAD_VALUE (65536-(CLOCK/12/TIMER_FREQ_HZ))
-#define RELOAD_VALUE_H ((unsigned char)(RELOAD_VALUE>>8))
-#define RELOAD_VALUE_L ((unsigned char)(RELOAD_VALUE))
-
-void initTimer(void)			// Функция инициализации таймера
+void ExecuteTask1() // ввод пользователем даты и времени
 {
-	TMOD=0x01;
-	TL0 = RELOAD_VALUE_L;
-	TH0 = RELOAD_VALUE_H;
-	ET0 = 1;
-	TR0 = 1;
+	unsigned char hour, minute, second;
+	unsigned char day, month, year;
+
+	printf("Enter hour: ");
+	scanf("%bu", &hour);
+	printf("Enter minute: ");
+	scanf("%bu", &minute);
+	printf("Enter second: ");
+	scanf("%bu", &second);
+
+	DS1307_settime(hour, minute, second);
+
+	printf("Enter day: ");
+	scanf("%bu", &day);
+	printf("Enter month: ");
+	scanf("%bu", &month);
+	printf("Enter year: ");
+	scanf("%bu", &year);
+
+	DS1307_setdate(day, month, year);
 }
-void led(void) interrupt 1 using 0		//Функция, вызываемая по прерыванию
+
+void ExecuteTask2() // вывод даты и времени на экран
 {
-	x++;
-	s++;
-	key = KEY_getkey();
-	if (key)
+	char time[9] = {0};
+	char date[9] = {0};
+	static char sec = 0;
+
+	DS1307_gettime(time);
+	if (time[7] == sec)
 	{
-		y = KEY_getkeynumber(key);
-		a=y;
+		return;
+	}
+	sec = time[7];
+
+	DS1307_getdate(date);
+
+	LCD_gotoxy(0, 0);
+	LCD_print(time);
+
+	LCD_gotoxy(0, 1);
+	LCD_print(date);
+}
+
+void ExecuteTask3() // очистка экрана
+{
+	LCD_clrscr();
+}
+
+// LCD
+
+void OutputStartLCDMessage()
+{
+	unsigned char mes_line1[] = "task 2";
+
+	LCD_print(mes_line1);
+}
+
+// UART
+
+void Serial_ISR(void) interrupt 4 using 0 // обработчик прерываний от UART
+{
+	if (RI)
+	{
+
+		scanf("%bu", &task);
+		switch (task)
+		{
+		case 1:
+			printf("task 1");
+			ExecuteTask1();
+			break;
+		}
+		printf("task number");
+		printf("%c", task);
 	}
 }
-void main()				//Основная функция
+
+void main()
 {
-	P0 = 0xFF;			//Иниц LED, LCD, SPI, I2C, прерывания разрешены
+
+	P0 = 0xFF;
 	P1 = 0xFF;
-	P2 = 0xFF;	
+	P2 = 0xFF;
 	P3 = 0xFF;
-	EA=1;
-	initTimer();
-	SPI_init();    
-	I2C_init();	
-	LCD_init();				
-	LCD_clrscr();	
-	LCD_gotoxy( 0, 0 );		//Вывод номера задачи
-	LCD_print( "Zadacha_1" );
-		while(1)
+
+	SCON = 0x50;
+	TMOD = 0x20;
+	TH1 = 250;
+	TL1 = 250;
+	TR1 = 1;
+	TI = 1;
+	ES = 1; // разрешение прерываний от UART
+
+	printf("                -=  STEND RK-10  =-\n");
+	printf("       Hardware ver. 1.0, Firmware ver. 1.0\n");
+
+	DELAY_MCS(174);
+
+	SPI_init(); // иниц SPI, I2C, DS1307, LCD
+
+	I2C_init();
+	DS1307_init();
+	DS1820_startconverttemp();
+
+	LCD_init();	  //  LCD
+	LCD_clrscr(); // LCD
+
+	OutputStartLCDMessage();
+
+	printf("Enter task number:");
+
+	EA = 1;
+	while (1)
 	{
-		if(x > 50)   		//цикл мигания LED для проверки работы прерываний
+		if (task == 2)
 		{
-			LED1 = !LED1;
-			x = 1;
+			ExecuteTask2();
 		}
-		LED7_setdigit(a); 	//семисегментник
-		delay_ms  (50);
-		LCD_gotoxy( 0, 1 );
-		if (y != "")
+		if (task == 3)
 		{
-			if (key == KEY_ASTERISK)		//очистка экрана
-				{
-					LCD_clrscr();
-					LCD_gotoxy( 0, 0 );
-					LCD_print( "Zadacha_4" );
-					z = 0;
-				}
-			if (key == KEY_BAR || (y>9 && y<14))  //сигнал по нажатию
-				{
-					while (i<3)
-					{
-						s = 0;
-						while (s<30)
-						{
-							BEEP_BIT = 0;
-						}
-						BEEP_BIT = 1;
-						s = 0;
-						while (s<10)
-						{
-							BEEP_BIT = 1;
-						}
-						i++;
-					}
-					i = 0;
-					s = 0;
-					while (s<20)
-						{
-							BEEP_BIT = 1;
-						}
-					while (i<3)
-					{
-						s = 0;
-						while (s<10)
-						{
-							BEEP_BIT = 0;
-						}
-						BEEP_BIT = 1;
-						s = 0;
-						while (s<10)
-						{
-							BEEP_BIT = 1;
-						}
-						i++;
-					}
-					i = 0;
-					s = 0;
-					while (s<20)
-						{
-							BEEP_BIT = 1;
-						}
-					while (i<3)
-					{
-						s = 0;
-						while (s<30)
-						{
-							BEEP_BIT = 0;
-						}
-						BEEP_BIT = 1;
-						s = 0;
-						while (s<10)
-						{
-							BEEP_BIT = 1;
-						}
-						i++;
-					}
-					i = 0;
-					s = 0;
-					while (s<20)
-						{
-							BEEP_BIT = 1;
-						}
-						y = "";	
-				}
-			if (y <= 9) 		//вывод на экран при нажатии
-			{
-				if( z< 16 )
-				{
-					LCD_gotoxy( z, 1 );
-					LCD_printchar (y + '0');
-					z++;
-					y = "";
-				}
-				else
-				{
-					z = 0;
-					LCD_gotoxy( z, 1 );
-					LCD_printchar (y + '0');
-					z++;
-					y = "";	
-				}
-			}
-		KEY_waitforkeyrelease();
+			ExecuteTask3();
 		}
+		delay_ms(5);
 	}
+}
